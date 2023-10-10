57a596e24cc4efca0f971ceaa8b7b692b33cc4d1
julio <jcolladopm7@gmail.com>
2023-10-10 18:30:14 -0400

    base files

---
+++ Sources/Themezer.xcframework/ios-arm64_x86_64-simulator/Themezer.framework/Headers/Themezer.h
@@ -0,0 +1,18 @@
+//
+//  Themezer.h
+//  Themezer
+//
+//  Created by Julio Collado Perez on 10/10/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Themezer.
+FOUNDATION_EXPORT double ThemezerVersionNumber;
+
+//! Project version string for Themezer.
+FOUNDATION_EXPORT const unsigned char ThemezerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Themezer/PublicHeader.h>
+
+
